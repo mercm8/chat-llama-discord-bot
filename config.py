8234efb7aa4910e5a625d613307d490ff63ea3d9
mercm8 <37743453+mercm8@users.noreply.github.com>
2023-05-17 15:38:57 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 discord = {'TOKEN': "YOURDISCORDTOKENHERE" }
-sd = {'A1111' : "http://127.0.0.1:7860"}
+sd = {'A1111' : "http://127.0.0.1:7860"} #This is the default URL for the A1111 API. If you don't have one, dont worry about it.
