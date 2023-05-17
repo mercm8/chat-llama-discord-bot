45266d56c87ee699544bea561a527b2f876719ca
mercm8 <magnus@mordt.net37743453+mercm8@users.noreply.github.com>
2023-05-17 15:36:58 +0200

    config changes

--- bot.py
+++ bot.py
@@ -19,10 +19,10 @@ from PIL import Image, PngImagePlugin
 import requests
 import sqlite3
 
-### Edit config.py to include your Discord token.
+### Edit config.py to include your Discord token, and A1111 api URL if necessary.
 import config
 TOKEN = config.discord['TOKEN'] 
-A1111 = "http://127.0.0.1:7860"
+A1111 = config.discord['A1111'] 
 
 logging.basicConfig(format='%(levelname)s [%(asctime)s]: %(message)s (Line: %(lineno)d in %(funcName)s, %(filename)s )',
                     datefmt="%a, %d %b %Y %H:%M:%S +0000", 
